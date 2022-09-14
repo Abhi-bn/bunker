f106adb0ca6663033e0d7bd5c53a805e964afc1e
Abhinava Nanjundaiah <bnabhi96@gmail.com>
2022-09-14 01:08:32 -0500

    Project Setup

---
+++ src/MainTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertEquals("This is Bunker", "This " + "is Bunker");
+    }
+}
